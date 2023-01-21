73cbe7bb172bfabc4d0149d8a771570883295d68
yagnesh25gehlot <yagneshgehlot1998@gmail.com>
2023-01-21 14:18:08 +0530

    hellow bitch

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     system.out.println('Hello World!');
+    system.out.println("Hello kartikey");
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
